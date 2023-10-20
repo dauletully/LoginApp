76881b6b8dd163fcd3ffa3d09974f86f89593879
Nurlybakyt <begaly606@gmail.com>
2023-10-20 15:43:47 +0600

    created label Login

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  LoginApp
-//
-//  Created by Dinmukhammed Begaly on 17.10.2023.
-//
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
 
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.textColor = .blue
+        label.font = .boldSystemFont(ofSize: 35)
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
 
+    private func setupViews(){
+        view.addSubview(titleLabel)
+    }
 
+    private func setupConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(50)
+            make.leading.equalToSuperview().offset(145)
+        }
+    }
 }
 
