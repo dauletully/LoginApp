0cf8c6db6c92c8a6cb8f613a09c130ce7af62375
Nurlybakyt <begaly606@gmail.com>
2023-10-20 19:15:01 +0600

    Created login and password field

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -10,6 +10,25 @@ class ViewController: UIViewController {
         label.font = .boldSystemFont(ofSize: 35)
         return label
     }()
+
+    private lazy var textFieldLogin: UITextField = {
+        let login = UITextField()
+        login.borderStyle = .roundedRect
+        login.textColor = .black
+        login.textAlignment = .left
+        login.placeholder = "Enter you email"
+        return login
+    }()
+
+    private lazy var textFieldPassword: UITextField = {
+        let password = UITextField()
+        password.borderStyle = .roundedRect
+        password.textAlignment = .left
+        password.placeholder = "Enter password"
+        password.isSecureTextEntry = true
+        return password
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -17,17 +36,36 @@ class ViewController: UIViewController {
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        textFieldLogin.layer.cornerRadius = 45
+        textFieldPassword.layer.cornerRadius = 45
     }
+
+    //MARK: - Setup Views
 
     private func setupViews(){
         view.addSubview(titleLabel)
+        view.addSubview(textFieldLogin)
+        view.addSubview(textFieldPassword)
     }
 
     private func setupConstraints() {
         titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(50)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(80)
             make.leading.equalToSuperview().offset(145)
+        }
+        textFieldLogin.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(80)
+            make.leading.equalToSuperview().offset(40)
+            make.trailing.equalToSuperview().offset(-39)
+            make.height.equalTo(40)
+            make.width.equalTo(300)
+        }
+        textFieldPassword.snp.makeConstraints { make in
+            make.top.equalTo(textFieldLogin.snp.bottom).offset(10)
+            make.leading.equalToSuperview().offset(40)
+            make.trailing.equalToSuperview().offset(-39)
+            make.height.equalTo(40)
+            make.width.equalTo(300)
         }
     }
 }
-
